6cc12a4b0ddb9179d9f77a3396ca1848ff790102
omarxar71 <omarabdrabi123@gmail.com>
2026-04-07 20:56:48 +0200

    Update: added new feature and fixed bugs

--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -1,16 +1,21 @@
 import User from '../../database/User/user.model.js';
 export const updateCandidateProfileDetailsAfterLogIn = async(req , res , next)=>{
+   try {
     const {candidateProfile:
-    {
-        specialization , experienceLevel ,expectedSalary ,workType ,cvUrl,skills ,status , role 
-    } }=req.body;
-    const user = await User.findByIdAndUpdate(req.user.id , {
-        candidateProfile:{
-            specialization , experienceLevel ,expectedSalary ,workType ,cvUrl,skills ,status ,role
-        }
-    } , {new:true})
-   
-    return res.status(200).json({message:"user profile details updated successfully" , user});
+        {
+            specialization , experienceLevel ,expectedSalary ,workType ,cvUrl,skills ,status , role 
+        } }=req.body;
+        const user = await User.findByIdAndUpdate(req.user.id , {
+            candidateProfile:{
+                specialization , experienceLevel ,expectedSalary ,workType ,cvUrl,skills ,status ,role
+            }
+        } , {new:true})
+       
+        return res.status(200).json({message:"user profile details updated successfully" , user});
+   } catch (error) {
+    return res.status(500).json({message : "internal server error" , error : error.message})
+    
+   }
 }
 
 //this is the company employer not our system employer
