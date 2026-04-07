9fa3f2c371a8c03ee52f2bea5319acb9fea93306
omarxar71 <omarabdrabi123@gmail.com>
2026-04-07 20:47:45 +0200

    Update: added new feature and fixed bugs

--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -2,16 +2,14 @@ import User from '../../database/User/user.model.js';
 export const updateCandidateProfileDetailsAfterLogIn = async(req , res , next)=>{
     const {candidateProfile:
     {
-        specialization , experienceLevel ,expectedSalary ,workType ,cvUrl,skills ,status 
+        specialization , experienceLevel ,expectedSalary ,workType ,cvUrl,skills ,status , role 
     } }=req.body;
     const user = await User.findByIdAndUpdate(req.user.id , {
         candidateProfile:{
-            specialization , experienceLevel ,expectedSalary ,workType ,cvUrl,skills ,status 
+            specialization , experienceLevel ,expectedSalary ,workType ,cvUrl,skills ,status ,role
         }
     } , {new:true})
-    if(!user || user.role !== "candidate"){
-        return res.status(404).json({message:"user not found or user is not a candidate"});
-    }
+   
     return res.status(200).json({message:"user profile details updated successfully" , user});
 }
 
